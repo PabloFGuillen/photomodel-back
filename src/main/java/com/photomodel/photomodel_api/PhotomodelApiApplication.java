94386160a4d9b0392f5c757114e430735bdb8609
Pablo <pablofguillen1503@gmail.com>
2025-09-02 23:04:36 +0200

    Inicializamos proyecto back

---
+++ src/main/java/com/photomodel/photomodel_api/PhotomodelApiApplication.java
@@ -0,0 +1,13 @@
+package com.photomodel.photomodel_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotomodelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotomodelApiApplication.class, args);
+	}
+
+}
